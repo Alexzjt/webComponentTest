225b763548499a04875b6f97095b526a8c60cf0c
zhangjingtao <alex_zjt@163.com>
2019-10-27 19:37:46 +0800

    111

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {DoBootstrap, Injector, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import {createCustomElement} from '@angular/elements';
+import zh from '@angular/common/locales/zh';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap{
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap() {
+    const elm = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('custom-element', elm);
+  }
+}
